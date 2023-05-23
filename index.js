b21d9d0c2f54618d8b9ef9ce63731e0ef6c15b40
unknown <loleste.lolo@gmail.com>
2023-05-23 14:41:49 +0200

    Ajout de la suppression d'un cours

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express= require('express')
 const app =express()
 const parkings = require('./cours.json')
-// Routes
+
+// Affichage de tous les cours
 app.get('/cours', (req, res) => { res.status(200).json(parkings) })
+
+// Affichage d'un cour en particulier
 app.get('/cours/:id', (req, res) => {
 const id = parseInt(req.params.id)
 const parking = parkings.find(parking => parking.id === id)
@@ -10,7 +13,13 @@ res.status(200).json(parking)
 })
 
 
+// Suppression d'un cours de tennis
+app.delete('/cours/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    let parking = parkings.find(parking => parking.id === id)
+    parkings.splice(parkings.indexOf(parking), 1)
+    res.status(200).json(parkings)
+})
 
-
-
+// Permet de nous informer dans la console que le serveur en écoute
 app.listen(8080, () => { console.log("Serveur à l'écoute") })
