a29a4b52cd39c3372e6a51351de5ac8f7a0feb37
unknown <loleste.lolo@gmail.com>
2023-05-23 15:25:20 +0200

    Afficher les détails d'une réservation en particulier

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
 const express= require('express')
 const app =express()
-const parkings = require('./cours.json')
+const cours = require('./cours.json')
+const reservations = require('./reservations.json')
 
 // Affichage de tous les cours
-app.get('/cours', (req, res) => { res.status(200).json(parkings) })
+app.get('/cours', (req, res) => { res.status(200).json(cours) })
 
 // Affichage d'un cour en particulier
 app.get('/cours/:id', (req, res) => {
 const id = parseInt(req.params.id)
-const parking = parkings.find(parking => parking.id === id)
-res.status(200).json(parking)
+const cour = cours.find(cour => cour.id === id)
+res.status(200).json(cour)
 })
-
 
 // Suppression d'un cours de tennis
 app.delete('/cours/:id', (req, res) => {
     const id = parseInt(req.params.id)
-    let parking = parkings.find(parking => parking.id === id)
-    parkings.splice(parkings.indexOf(parking), 1)
-    res.status(200).json(parkings)
+    let cours = cours.find(cours => cours.id === id)
+    cours.splice(cours.indexOf(cours), 1)
+    res.status(200).json(cours)
 })
+
+// Prendre une réservation pour un cours de tennis
+app.post('/reservations', (req, res) => {
+    console.log(req.body)
+    reservations.push(req.body) 
+    res.status(200).json(reservations)
+})
+
+// Lister l'ensemble des réservations
+app.get('/reservations', (req, res) => { res.status(200).json(reservations) })
+
+// Afficher les détails d'une réservation en particulier 
+app.get('/reservations/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const reservation = reservations.find(reservation => reservation.id === id)
+    res.status(200).json(reservation)
+    })
 
 // Permet de nous informer dans la console que le serveur en écoute
 app.listen(8080, () => { console.log("Serveur à l'écoute") })
