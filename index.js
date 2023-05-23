557cf6ae5b7eb6c36e337cb1597b7ab302b67443
unknown <loleste.lolo@gmail.com>
2023-05-23 14:31:54 +0200

    Lister l'ensemble des cours

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express= require('express')
 const app =express()
+const parkings = require('./cours.json')
+// Routes
+app.get('/cours', (req,res) => { res.status(200).json(parkings)})
+
+
 
 app.listen(8080, () => { console.log("Serveur à l'écoute") })
